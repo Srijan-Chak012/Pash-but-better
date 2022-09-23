d3587de611cc43adce29ca1a5b54cf169c6bd5a4
“Srijan-Chak012” <srijan.chakraborty@research.iiit.ac.in>
2022-09-24 02:10:23 +0530

    Command

--- fgbg.c
+++ fgbg.c
@@ -3,145 +3,260 @@
 int argc, bg_no;
 char *argv[265];
 int background;
+char *actions = "command not found";
 
-void get_args(char *token) {
+void get_args(char *token)
+{
     argc = 0;
     background = 0;
-    while (token != NULL) {
+    while (token != NULL)
+    {
         argv[argc] = token;
         token = strtok(NULL, delim);
         ++argc;
     }
     argv[argc] = NULL;
-    if (strncmp(argv[argc - 1], "&", strlen(argv[argc - 1])) == 0) {
+    if (strncmp(argv[argc - 1], "&", strlen(argv[argc - 1])) == 0)
+    {
         background = 1;
         argv[argc - 1] = NULL;
         argc--;
     }
-}   
+}
 
-void fgbg(char * tkn) {
+void fgbg(char *tkn)
+{
     get_args(tkn);
 
     pid_t pid = fork();
-    
-    if (pid == -1) {
+
+    if (pid < 0)
+    {
         perror("Pash: fork");
-    } else if (pid == 0) {
+    }
+    else if (pid < 1 && pid > -1)
+    {
         setpgid(0, 0);
-        if (execvp(argv[0], argv) == -1) {
-            printf("%s: command not found\n", argv[0]);
+        if (execvp(argv[0], argv) < 0)
+        {
+            printf("%s: %s\n", argv[0], actions);
         }
-        _exit(EXIT_SUCCESS);
-    } else {
+        _exit(0);
+    }
+    else
+    {
         insertProcess(pid, argv[0]);
         setpgid(pid, pid);
-        if (!background) {
+        int counter = 0;
+        if (!background)
+        {
             int status;
             signal(SIGTTOU, SIG_IGN);
             signal(SIGTTIN, SIG_IGN);
-            tcsetpgrp(STDIN_FILENO, getpgid(pid));
-            tcsetpgrp(STDOUT_FILENO, getpgid(pid));
+            if (counter == 0)
+            {
+                tcsetpgrp(STDIN_FILENO, getpgid(pid));
+                tcsetpgrp(STDOUT_FILENO, getpgid(pid));
+                // printf("counter: %d", counter);
+            }
+
+            counter++;
 
             waitpid(pid, &status, WUNTRACED);
 
-            tcsetpgrp(STDIN_FILENO, getpgrp());
-            tcsetpgrp(STDOUT_FILENO, getpgrp());
-            signal(SIGTTIN, SIG_DFL);
-            signal(SIGTTOU, SIG_DFL);
+            if (counter == 1)
+            {
+                tcsetpgrp(STDIN_FILENO, getpgrp());
+                tcsetpgrp(STDOUT_FILENO, getpgrp());
+            }
+            // printf("Counter: ", counter);
 
-            if (!WIFSTOPPED(status)) {
+            counter++;
+            if (counter == 2)
+            {
+                signal(SIGTTIN, SIG_DFL);
+                signal(SIGTTOU, SIG_DFL);
+            }
+            // printf("Counter: ", counter);
+            if (WIFSTOPPED(status) == 0)
+            {
                 deleteProcess(pid);
             }
-        } else {
+        }
+        else
+        {
             printf("%d\n", pid);
         }
     }
-    for (int i = 0; i < argc; ++i) {
+    for (int i = 0; i < argc; ++i)
+    {
         argv[i] = NULL;
     }
-} 
+}
 
-void _foreground(int jobNo) {
+void _foreground(int jobNo)
+{
     checkJobs();
     int jobNoSpecified = 1;
-    if (jobNo == 0) {
+    if (jobNo == 0)
+    {
         jobNoSpecified = 0;
-        for (process* curr = process_ptr; curr != NULL; curr = curr->next) {
+        // for (process *curr = process_ptr; curr != NULL; curr = curr->next)
+        // {
+        //     jobNo = max(jobNo, curr->JobNo);
+        // }
+        process *curr = process_ptr;
+        while (curr != NULL)
+        {
             jobNo = max(jobNo, curr->JobNo);
+            curr = curr->next;
         }
     }
     pid_t childProcessPID = -1;
     int validJob = 0;
     char *name;
-    for (process* curr = process_ptr; curr != NULL; curr = curr->next) {
-        if (curr->JobNo == jobNo) {
+    // for (process *curr = process_ptr; curr != NULL; curr = curr->next)
+    // {
+    //     if (curr->JobNo == jobNo)
+    //     {
+    //         validJob = 1;
+    //         childProcessPID = curr->pid;
+    //         name = curr->name;
+    //         break;
+    //     }
+    // }
+    process *curr = process_ptr;
+    while (curr != NULL)
+    {
+        if (curr->JobNo == jobNo)
+        {
             validJob = 1;
-            childProcessPID = curr->pid; 
+            childProcessPID = curr->pid;
             name = curr->name;
             break;
         }
-    } 
-    if (!validJob) {
+        curr = curr->next;
+    }
+    if (validJob == 0)
+    {
         printf("Pash: fg: ");
-        if (jobNoSpecified) {
-            printf("%d: ", jobNo);
-        } else {
+        if (!jobNoSpecified)
+        {
             printf("current: ");
+        }
+        else
+        {
+            printf("%d: ", jobNo);
         }
         printf("No such job\n");
         return;
     }
-    signal(SIGTTOU, SIG_IGN);
-    signal(SIGTTIN, SIG_IGN);
-    tcsetpgrp(STDIN_FILENO, getpgid(childProcessPID));
-    tcsetpgrp(STDOUT_FILENO, getpgid(childProcessPID));
+
+    int counter = 0;
+
+    if (counter == 0)
+    {
+        signal(SIGTTOU, SIG_IGN);
+        signal(SIGTTIN, SIG_IGN);
+    }
+
+    counter++;
+
+    if (counter == 1)
+    {
+        tcsetpgrp(STDIN_FILENO, getpgid(childProcessPID));
+        tcsetpgrp(STDOUT_FILENO, getpgid(childProcessPID));
+    }
+    // printf("counter: %d", counter);
     int status, err = 0;
-    if (kill(childProcessPID, SIGCONT) == -1) {
-        err = 1;
+    if (kill(childProcessPID, SIGCONT) < 0)
+    {
         perror("Pash: fg");
-    } else {
+        err = 1;
+    }
+    else
+    {
         printf("%s\n", name);
         waitpid(childProcessPID, &status, WUNTRACED);
     }
-    tcsetpgrp(STDIN_FILENO, getpgrp());
-    tcsetpgrp(STDOUT_FILENO, getpgrp());
-    signal(SIGTTOU, SIG_DFL);
-    signal(SIGTTIN, SIG_DFL);
 
-    if (!WIFSTOPPED(status) && err) {
+    if (counter == 1)
+    {
+        tcsetpgrp(STDIN_FILENO, getpgrp());
+        tcsetpgrp(STDOUT_FILENO, getpgrp());
+    }
+    // printf("Counter: ", counter);
+
+    counter++;
+    if (counter == 2)
+    {
+        signal(SIGTTIN, SIG_DFL);
+        signal(SIGTTOU, SIG_DFL);
+    }
+    // printf("Counter: ", counter);
+
+    if (WIFSTOPPED(status) == 0 && err)
+    {
         deleteProcess(childProcessPID);
     }
 }
 
-void _background(int jobNo) {
+void _background(int jobNo)
+{
     checkJobs();
     int jobNoSpecified = 1;
-    if (jobNo == 0) {
+    if (jobNo == 0)
+    {
         jobNoSpecified = 0;
-        for (process* curr = process_ptr; curr != NULL; curr = curr->next) {
+        // for (process *curr = process_ptr; curr != NULL; curr = curr->next)
+        // {
+        //     jobNo = max(jobNo, curr->JobNo);
+        // }
+        process *curr = process_ptr;
+        while (curr != NULL)
+        {
             jobNo = max(jobNo, curr->JobNo);
+            curr = curr->next;
         }
     }
     pid_t childProcessPID = -1;
     int validJob = 0;
-    for (process* curr = process_ptr; curr != NULL; curr = curr->next) {
-        if (curr->JobNo == jobNo) {
+    // for (process *curr = process_ptr; curr != NULL; curr = curr->next)
+    // {
+    //     if (curr->JobNo == jobNo)
+    //     {
+    //         validJob = 1;
+    //         childProcessPID = curr->pid;
+    //         break;
+    //     }
+    // }
+    process *curr = process_ptr;
+    while (curr != NULL)
+    {
+        if (curr->JobNo == jobNo)
+        {
             validJob = 1;
-            childProcessPID = curr->pid; break;
+            childProcessPID = curr->pid;
+            break;
         }
-    } 
-    if (!validJob) {
+        curr = curr->next;
+    }
+    if (!validJob)
+    {
         printf("Pash: bg: ");
-        if (jobNoSpecified) {
+        if (jobNoSpecified)
+        {
             printf("%d: ", jobNo);
-        } else {
+        }
+        else
+        {
             printf("current: ");
         }
         printf("No such job\n");
         return;
     }
-    if (kill(childProcessPID, SIGCONT) == -1) {
+    if (kill(childProcessPID, SIGCONT) < 0)
+    {
         perror("Pash: bg");
     }
 }
